0ded8de959ec4436a0eab3c204b7076a55914f33
Wilfrido Almache <walmache@gmail.com>
2025-01-31 11:25:39 -0500

    Fix gunicorn settings and lazy load model

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,5 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD ["gunicorn", "--bind", "0.0.0.0:8080", "app.app:app"]
+#CMD ["gunicorn", "--bind", "0.0.0.0:8080", "app.app:app"]
+CMD ["gunicorn", "--workers=1", "--threads=4", "--timeout=120", "--bind", "0.0.0.0:8080", "app.app:app"]
--- app/app.py
+++ app/app.py
@@ -5,15 +5,24 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # Cargar modelo entrenado
-with open('model/model.pkl', 'rb') as f:
-    model = pickle.load(f)
+#with open('model/model.pkl', 'rb') as f:
+#    model = pickle.load(f)
+
+def load_model():
+    with open('model/model.pkl', 'rb') as f:
+        return pickle.load(f)
 
 @app.route('/predict', methods=['POST'])
 
 def predict():
+    # data = request.json['data']
+    # prediction = np.argmax(model.predict([data]), axis=1)
+    # return jsonify({'prediction': int(prediction[0])})
+    model = load_model()  # Se carga solo cuando se necesita
     data = request.json['data']
     prediction = np.argmax(model.predict([data]), axis=1)
     return jsonify({'prediction': int(prediction[0])})
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
